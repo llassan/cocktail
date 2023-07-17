61cf61898213cbc5fa10a49f82d82aea7e60791e
llassan <126498248+llassan@users.noreply.github.com>
2023-07-17 12:37:33 +0530

    Add files via upload

---
+++ src/Components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="mt-4 bg-dark text-white">
+      <div className="container p-2 text-center">
+        <h5>Design And Developed By : Vikash Kumar </h5>
+        <h6>&copy; All Right Reserved</h6>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
